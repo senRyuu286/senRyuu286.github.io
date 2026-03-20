5c035a893524542ffdab9a2615f56fa9a381b980
Justin B. Ramas <142135986+senRyuu286@users.noreply.github.com>
2026-03-20 22:34:26 +0800

    Add GitHub Pages SPA redirect and 404 page
    Enable SPA routing on GitHub Pages and improve 404 UX. Added the spa-github-pages redirect script to index.html to restore client-side routes from GitHub Pages redirects. Replaced public/404.html with a styled Tailwind-based 404 page that shows a message and auto-redirects back to the site (preserving the original path via query param). Set BrowserRouter basename to "/" in App.tsx and cleaned up old commented code. Also renamed Home.jsx and About.jsx to Home.tsx and About.tsx (TypeScript migration).

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,6 @@
-// @ts-ignore: allow importing JS module without a declaration file
-// import WorkInProgress from "./pages/WorkInProgress";
-
-// function App() {
-//   return <WorkInProgress />;
-// }
-
-// export default App;
-
-// import NavigationBar from "./components/NavigationBar";
-// import HeroPage from "./components/HeroPage";
-
-// function App() {
-//   return (
-//     <div>
-//       <NavigationBar />
-//       <HeroPage />
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// // @ts-ignore: allow importing JS module without a declaration file
-// import Home from "./pages/Home";
-// // @ts-ignore: allow importing JS module without a declaration file
-// import About from "./pages/About";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
-
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-// @ts-ignore: allow importing JS module without a declaration file
 import Home from "./pages/Home";
-// @ts-ignore: allow importing JS module without a declaration file
 import About from "./pages/About";
 
 function AnimatedRoutes() {
@@ -63,7 +18,7 @@ function AnimatedRoutes() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <AnimatedRoutes />
     </BrowserRouter>
   );
